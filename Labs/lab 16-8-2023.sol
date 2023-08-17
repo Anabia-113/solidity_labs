39f0c771116d1a89d9607de6aef975b585a11c38
Anabia-113 <baiganabia113@gmail.com>
2023-08-17 05:29:50 -0700

    create

--- Labs/lab 16-8-2023.sol
+++ Labs/lab 16-8-2023.sol
@@ -223,28 +223,287 @@ pragma solidity ^0.8.9;
 // }
 
 //task 13
-contract Base {
-    uint256 public x;
+// contract Base {
+//     uint256 public x;
 
-    function setX(uint256 _x) public {
-        x = _x;
+//     function setX(uint256 _x) public {
+//         x = _x;
+//     }
+// }
+
+// contract DerivedA is Base {
+//     function getX() public view returns (uint256) {
+//         return x;
+//     }
+// }
+
+// contract DerivedB is Base {
+//     function getXTimesTwo() public view returns (uint256) {
+//         return x * 2;
+//     }
+// }
+// contract DerivedC is Base {
+//     function getXCube() public view returns (uint256) {
+//         return x ** 3;
+//     }
+// }
+
+//task 14
+// contract car{
+//      string public make;
+//      string public model;
+
+//     constructor(string memory _make, string memory _model) {
+//         make = _make;
+//         model = _model;
+//     }
+
+// }
+// contract electricCar is car{
+//     uint256 public batteryCapacity;
+
+//     constructor(
+//         string memory _make,
+//         string memory _model,
+//         uint256 _batteryCapacity
+//     )
+//      car(_make, _model) {
+//         batteryCapacity = _batteryCapacity;
+//     }
+
+// }
+// contract hybridCar is car{
+//        uint256 public gasTankCapacity;
+
+//     constructor(
+//         string memory _make,
+//         string memory _model,
+//         uint256 _gasTankCapacity
+//     )
+//      car(_make, _model) {
+//         gasTankCapacity = _gasTankCapacity;
+//     }
+
+// }
+// contract Instances {
+//     electricCar public electricCarInstance;
+//     hybridCar public hybridCarInstance;
+
+//     constructor() {
+//         electricCarInstance = new electricCar("ModelE", "Tesla", 500);
+//         hybridCarInstance = new hybridCar("ModelF", "Toyota", 50);
+//     }
+
+//     function getElectricCarAttributes() public view returns (string memory, string memory, uint) {
+//         return (
+//             electricCarInstance.model(),
+//             electricCarInstance.make(),
+//             electricCarInstance.batteryCapacity()
+//         );
+//     }
+
+//     function getHybridCarAttributes() public view returns (string memory, string memory, uint) {
+//         return (
+//             hybridCarInstance.model(),
+//             hybridCarInstance.make(),
+//             hybridCarInstance.gasTankCapacity()
+//         );
+//     }
+// }
+//task 15
+// contract engine{
+//     uint public horsePower;
+//     constructor(uint _horsePower) {
+//         horsePower = _horsePower;
+//     }
+
+// }
+// contract transmission{
+//     uint public gearType;
+
+//     constructor(uint _gearType) {
+//         gearType = _gearType;
+//     }
+
+// }
+// contract car is engine , transmission{
+//      constructor(uint _horsepower, uint _gearType)
+//         engine(_horsepower)
+//         transmission(_gearType)
+//     {
+//     }
+
+//     // A function to get combined attributes
+//     function getCarAttributes() public view returns (uint, uint) {
+//         return (horsePower, gearType);
+//     }
+
+// }
+
+//task 16
+// contract WorkerRole {
+//     address[] public workers;
+//     mapping(address => bool) public isWorker;
+
+//     function addWorker(address _worker) external {
+//         isWorker[_worker] = true;
+//     }
+
+//     function submitWork() external pure {
+
+//     }
+// }
+
+// contract ClientRole {
+//     address[] public clients;
+//     mapping(address => bool) public isClient;
+
+//     function addClient(address _client) external {
+//         isClient[_client] = true;
+//     }
+
+//     function createProject() external pure {
+
+//     }
+// }
+// contract Freelancer is WorkerRole, ClientRole {
+//     constructor() {
+
+//     }
+// }
+
+// contract FreelancerInstance {
+//     Freelancer public freelancerInstance;
+
+//     constructor() {
+//         freelancerInstance = new Freelancer();
+//     }
+
+//     function addWorker(address _worker) public {
+//         freelancerInstance.addWorker(_worker);
+//     }
+
+//     function addClient(address _client) public {
+//         freelancerInstance.addClient(_client);
+//     }
+
+//     function submitWork() public view {
+//         freelancerInstance.submitWork();
+//     }
+
+//     function createProject() public view {
+//         freelancerInstance.createProject();
+//     }
+// }
+
+//task 15
+// contract Participant {
+//     address public participantAddress;
+
+//     constructor() {
+//         participantAddress = msg.sender;
+//     }
+// }
+
+// contract Supplier is Participant {
+//     mapping(address => uint256) public orders;
+
+//     function placeOrder(address _manufacturer, uint256 _quantity) external {
+//         orders[_manufacturer] += _quantity;
+//     }
+// }
+
+// contract Manufacturer is Participant {
+//     mapping(address => uint256) public manufacturedProducts;
+
+//     function manufactureProduct(uint256 _quantity) external {
+//         manufacturedProducts[participantAddress] += _quantity;
+//     }
+// }
+
+// contract Product is Supplier, Manufacturer {
+//     uint256 public productCount;
+
+//     constructor() {
+//         productCount = 0;
+//     }
+
+//     function trackProduct() external view returns (uint256, uint256) {
+//         return (
+//             orders[participantAddress],
+//             manufacturedProducts[participantAddress]
+//         );
+//     }
+// }
+
+// contract SupplyChain {
+//     Product public productInstance;
+
+//     constructor() {
+//         productInstance = new Product();
+//     }
+
+//     function placeOrder(uint256 _quantity) public {
+//         productInstance.placeOrder(address(this), _quantity);
+//     }
+
+//     function manufactureProduct(uint256 _quantity) public {
+//         productInstance.manufactureProduct(_quantity);
+//     }
+
+//     function trackProduct() public view returns (uint256, uint256) {
+//         return productInstance.trackProduct();
+//     }
+// }
+
+//task 16
+/* Inheritance tree：
+ Car
+ / \
+TOYOTA BMW
+ \ /
+ Supra
+*/
+contract Car {
+    event Log(string message);
+
+    function demo() public virtual {
+        emit Log("Base Car demo Called");
+    }
+
+    function temp() public virtual {
+        emit Log("Base Car temp Called");
     }
 }
 
-contract DerivedA is Base {
-    function getX() public view returns (uint256) {
-        return x;
+contract TOYOTA is Car {
+    function demo() public virtual override {
+        emit Log("TOYOTA demo called");
+    }
+
+    function temp() public virtual override {
+        emit Log("TOYOTA temp called");
+        super.temp();
     }
 }
 
-contract DerivedB is Base {
-    function getXTimesTwo() public view returns (uint256) {
-        return x * 2;
+contract BMW is Car {
+    function demo() public virtual override {
+        emit Log("BMW demo called");
     }
-}
-contract DerivedC is Base {
-    function getXTimesTwo() public view returns (uint256) {
-        return x * 3;
+
+    function temp() public virtual override {
+        emit Log("BMW temp called");
+        super.temp();
     }
 }
 
+contract Supra is TOYOTA, BMW {
+    function demo() public override(TOYOTA, BMW) {
+        super.demo();
+    }
+
+    function temp() public override(TOYOTA, BMW) {
+        super.temp();
+    }
+}
