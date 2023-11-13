06501a2f18a14d1b45300d445eaeac187371272e
Anabia-113 <baiganabia113@gmail.com>
2023-11-13 16:09:49 +0500

    create

--- Capstone project/payment.sol
+++ Capstone project/payment.sol
@@ -1,52 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// contract PaymentSystem {
-//     address public employer;
-//     address public employee;
-//     address public escrow;
-//     uint public amount;
-//     bool public employerSigned;
-//     bool public employeeSigned;
-
-//     constructor(address _employee, address _escrow, uint _amount) {
-//         employer = msg.sender;
-//         employee = _employee;
-//         escrow = _escrow;
-//         amount = _amount;
-//     }
-
-//     modifier onlyEmployer() {
-//         require(msg.sender == employer, "Only the employer can perform this action");
-//         _;
-//     }
-
-//     modifier onlyEmployee() {
-//         require(msg.sender == employee, "Only the employee can perform this action");
-//         _;
-//     }
-
-//     modifier onlyEscrow() {
-//         require(msg.sender == escrow, "Only the escrow can perform this action");
-//         _;
-//     }
-
-//     function employerSigns() public onlyEmployer {
-//         employerSigned = true;
-//     }
-
-//     function employeeSigns() public onlyEmployee {
-//         employeeSigned = true;
-//     }
-
-//     function transfer() public onlyEscrow  {
-//         require(employerSigned && employeeSigned, "Both parties must sign before transferring funds");
-
-//         // Transfer funds to the  employee's wallet
-//         payable(employee).transfer(amount);
-//     }
-// }
-
 contract PaymentSystem {
     address public employer;
     address public employee;
@@ -60,9 +14,9 @@ contract PaymentSystem {
     constructor()payable {
         owner=msg.sender;
     }
-
+     
     function initializePayment(address _employee, uint _amount) public {
-        require(employer == address(0), "Payment details are already initialized.");
+        require(employer != address(0), "Payment details are already initialized.");
         employer = msg.sender;
         employee = _employee;
         paymentAmount = _amount;
@@ -79,12 +33,12 @@ contract PaymentSystem {
     }
 
     function employerSigns() public onlyEmployer {
-        require(!employerSigned, "Employer has already signed.");
+        require(employerSigned, "Employer has already signed.");
         employerSigned = true;
     }
 
     function employeeSigns() public onlyEmployee {
-        require(!employeeSigned, "Employee has already signed.");
+        require(employeeSigned, "Employee has already signed.");
         employeeSigned = true;
     }
 
@@ -126,16 +80,3 @@ contract PaymentSystem {
         disputeRaised = false;
     }
 }
-//     event Err(uint);
-//     function deposit()public payable{
-//         if(msg.value<1000){
-//             emit Err(msg.value);
-//         }
-//     }
-//     function getBal()public view returns(uint){
-//         return address(this).balance;
-//     }
-//     function transfer()public payable{
-//         payable(0xc49b369cfD9D7Fcf7C643055bF4189BE010A1120).transfer(address(this).balance);
-//     }
-// }
