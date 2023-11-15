1f7a3478efad895ede2abbb9c6110a84ac5f67cb
Anabia-113 <baiganabia113@gmail.com>
2023-11-15 15:50:17 +0500

    create

--- Capstone project/payment.sol
+++ Capstone project/payment.sol
@@ -11,60 +11,71 @@ contract PaymentSystem {
     bool public disputeRaised;
     address public disputeParty;
     address private owner;
+
+    //// Constructor that sets the contract deployer as the owner
     constructor()payable {
         owner=msg.sender;
     }
+    //A mapping to store reviews given by the employer for the employee
      mapping(address=>string) public reviews;
-    function initializePayment(address _employee, uint _amount) public {
+
+    //Modifier to restrict certain functions to only the employer
+    modifier onlyEmployer() {
+        require(msg.sender == employer, "Only the employer can perform this action.");
+        _;
+    }
+    // Modifier to restrict certain functions to only the employee
+    modifier onlyEmployee() {
+        require(msg.sender == employee, "Only the employee can perform this action.");
+        _;
+    }
+    // Function to set the deployer as the employer
+     function IamEmployer()public{
+        employer=msg.sender;
+    }
+    // Function to set the caller as the employee
+    function Employ()public{
+      employee=msg.sender;
+    }
+    // Function to initialize payment details (can only be called once)
+     function initializePayment(address _employee, uint _amount) public {
         require(employer != address(0), "Payment details are already initialized.");
         employer = msg.sender;
         employee = _employee;
         paymentAmount = _amount;
     }
-
-    modifier onlyEmployer() {
-        require(msg.sender == employer, "Only the employer can perform this action.");
-        _;
-    }
-
-    modifier onlyEmployee() {
-        require(msg.sender == employee, "Only the employee can perform this action.");
-        _;
-    }
+    // Function for the employer to give a review for the employee
      function giveReview(string memory review)public onlyEmployer{
         reviews[employee]=review;
      }
+     // Function for the employer to sign the payment (can only be called once)
     function employerSigns() public onlyEmployer {
         require(employerSigned, "Employer has already signed.");
         employerSigned = true;
     }
-
+    // Function for the employee to sign the payment (can only be called once)
     function employeeSigns() public onlyEmployee {
         require(employeeSigned==true, "Employee has already signed.");
         employeeSigned = true;
     }
+    // Function to get the review given by the employer for the employee
     function getReview()public view returns(string memory){
         return reviews[employee];
     }
-
+    // Function for the employee to mark the work as completed
     function completeWork() public onlyEmployee {
         require(!workCompleted, "Work is already completed.");
         workCompleted = employerSigned&&employeeSigned;
         reviews[msg.sender]="";
     }
-
+    // Function for either party to raise a dispute
     function raiseDispute() public {
         require(msg.sender == employer || msg.sender == employee, "Only the employer or employee can raise a dispute.");
         require(!disputeRaised, "A dispute is already raised.");
         disputeRaised = true;
         disputeParty = msg.sender;
     }
-    function IamEmployer()public{
-        employer=msg.sender;
-    }
-    function Employ()public{
-      employee=msg.sender;
-    }
+    // Function to resolve the dispute, transferring funds accordingly
     function resolveDispute(bool resolve) public {
         require(disputeRaised, "No dispute to resolve.");
 
